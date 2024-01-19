85f56c520e67443209e7c9d2592d2cdf82a1ae40
Pratiksha Kumbhare <127101758+Pratiksha426@users.noreply.github.com>
2024-01-19 14:48:00 +0530

    code.py

--- code.py
+++ code.py
@@ -1,1 +1,126 @@
+#importing important libraries
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.datasets import cifar10
+import PIL
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from tensorflow import keras
+
+# loading in the data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+
+print('Size of train data {}'.format(X_train.shape))
+print('Size of test data {}'.format(X_test.shape))
+
+print('Size of train class {}'.format(y_train.shape))
+print('Size of test class {}'.format(y_test.shape))
+
+class_names = ['airplane','automobile','bird','cat','deer',
+               'dog','frog','horse','ship','truck']
+num_classes = len(np.unique(y_train))
+
+print('Total class {}'.format(num_classes))
+
+#plotting first image of all 10 classes from train data
+plt.figure(figsize=(8, 8))
+for i in range(num_classes):
+    ax = plt.subplot(2, 5, i + 1)
+    idx = np.where(y_train[:]==i)[0]
+    features_idx = X_train[idx,::]
+    plt.imshow(features_idx[0])
+    ax.set_title(class_names[i])
+    plt.axis("off")
+
+# normalize the inputs from 0-255 to between 0 and 1 by dividing by 255
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+
+model = Sequential([
+  layers.Conv2D(32, (3,3),input_shape=X_train.shape[1:], padding='same', activation='relu'),
+  layers.MaxPooling2D(pool_size=(2,2)),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(pool_size=(2,2)),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(pool_size=(2,2)),
+  layers.Dropout(0.2),
+  layers.Flatten(),
+  layers.Dense(256, activation='relu'),
+  layers.Dense(num_classes, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+seed = 21
+np.random.seed(seed)
+epochs=10
+batch_size = 32
+history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=epochs, batch_size= batch_size)
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs_range = range(epochs)
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+plt.show()
+
+url_dict.clear()
+url_dict = {'Horse':'https://scx2.b-cdn.net/gfx/news/2020/1-geneticstudy.jpg',
+            'Car':'https://www.extremetech.com/wp-content/uploads/2019/12/SONATA-hero-option1-764A5360-edit.jpg',
+            'dog': 'https://i.insider.com/5df126b679d7570ad2044f3e?width=1800&format=jpeg&auto=webp',
+            'plane': 'https://www.netpaths.net/wp-content/uploads/google-airplane1.jpg',
+            'ship' : 'https://upload.wikimedia.org/wikipedia/commons/2/22/Diamond_Princess_%28ship%2C_2004%29_-_cropped.jpg'
+}
+i =0
+plt.figure(figsize=(8, 8))
+for key, value in url_dict.items():
+  url = value
+  i +=1
+  path = tf.keras.utils.get_file(key, origin=url)
+  img = keras.preprocessing.image.load_img(
+    path, target_size=(32, 32))
+  img_array = keras.preprocessing.image.img_to_array(img)
+  img_array = tf.expand_dims(img_array, 0) # Create a batch
+  predictions = model.predict(img_array)
+  score = tf.nn.softmax(predictions[0])
+  print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score)))
+  print('Image after resizing to 32x32')
+  ax = plt.subplot(1, 6, i + 1)
+  plt.imshow(img)
+  ax.set_title(class_names[np.argmax(score)])
+  plt.axis("off")
+
+
 
